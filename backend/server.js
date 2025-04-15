1b8669722c2d5298866e003d9d7a1f98c744c2d5
slimeVidas26 <dahan.isaac@gmail.com>
2025-04-15 20:43:07 +0300

    tuto2

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+//express app
+const app = express()
+
+//routes
+app.get('/', (req , res)=>{
+res.json({msg:"Welcome to the app"})
+})
+
+//listen for requests
+app.listen(4000 , ()=>{
+    console.log("Listen on port 4000")
+})
